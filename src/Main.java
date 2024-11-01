b6d6b2ad70f196024b2b6c8bfd2cc71e9be0c2b1
ksegura7 <147087643+ksegura7@users.noreply.github.com>
2024-11-01 13:03:23 -0500

    11/1/2024

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,36 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 interface Ingredient{
-    public String getName();
-    public double getQuantity();
+     String getName();
+     double getQuantity();
 }
 class SolidIngredient implements Ingredient{
     private String _name;
-    private double _quantity;
-    SolidIngredient(String n, int q){
-        _name = n;
-        _quantity = q;
-    }
+    private double _qtyInGrams;
+    SolidIngredient(String n, double q){_name = n;_qtyInGrams = q;}
     @Override
-    public String getName() {
-        return _name;
-    }
+    public String getName() {return _name;}
     @Override
-    public double getQuantity() {
-        return _quantity;
-    }
+    public double getQuantity() {return _qtyInGrams;}
 }
 class LiquidIngredient implements Ingredient{
     private String _name;
-    private double _quantity;
-    LiquidIngredient(String n, double q){
-        _name = n;
-        _quantity = q;
-    }
+    private double _qtyInMl;
+    LiquidIngredient(String n, double q){_name = n;_qtyInMl = q;}
     @Override
-    public String getName() {
-        return _name;
-    }
+    public String getName() {return _name;}
     @Override
-    public double getQuantity() {
-        return _quantity;
-    }
+    public double getQuantity() {return _qtyInMl;}
 }
-
+// Class recipe is not implementing the Ingredient interface.
+// rather, the extends word indicates a constraint for the type variable/parameter.
+// Extend is the only keyword you can use here.
 class Recipe<T extends Ingredient>{
     private String _name;
     private String _instructions;
     private ArrayList<T> _ingredients;
-    Recipe(String n, String i, ArrayList t){
+    Recipe(String n, String i){
         _name = n;
         _instructions = i;
         _ingredients = new ArrayList<T>();
@@ -50,8 +39,49 @@ class Recipe<T extends Ingredient>{
         _ingredients.add(i);
     }
     public void print(){
+            System.out.println("Recipe: " + _name);
+            System.out.println("Instruction: " + _instructions);
+            System.out.println("Ingredients:");
+            if (_ingredients != null){
+                for (T i : _ingredients){
+                    System.out.println("- " + i.getName() + ": " + i.getQuantity());
+                }
+            }
+
 
     }
 }
 public class Main{
+    private static int getChoice(Scanner s) {
+        int choice = 0;
+        System.out.println("RECIPE DATABASE");
+        System.out.println("1. Add ingredient to recipe");
+        System.out.println("2. List the ingredients of a recipe");
+        System.out.println("3. Exit the program");
+        System.out.print("Enter your choice: ");
+        try{
+            choice = Integer.valueOf(s.nextLine());
+        } catch (NumberFormatException e){
+            System.out.println("Invalid data entered" + e.getMessage());
+            System.exit(-1);
+        }
+        return choice;
+    }
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        //get recipe
+        //get instructions
+        //provide a simple example of how to conduct a unit test in intellj
+        int choice = getChoice(s);
+        while (choice != 3){
+            if (choice == 1){
+
+            } else {
+
+            }
+            choice = getChoice(s);
+        }
+    }
+
+
 }
